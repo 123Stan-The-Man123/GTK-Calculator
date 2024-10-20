2a84355c44fdc0a380853758ee39cb34efb4b3ce
123Stan-The-Man123 <Molsas2020@gmail.com>
2024-10-20 10:49:05 +0100

    Small change

--- install-script.sh
+++ install-script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+DESKTOP_DIR="$HOME/.local/share/applications/"
 TARGET_DIR="$HOME/.local/share/GTK-Calculator"
 
 mkdir -p "$TARGET_DIR"
+sudo cp ./src/Stans-Calculator.desktop "$DESKTOP_DIR"
 sudo cp -r . "$TARGET_DIR"
 
 echo "Source code installed in $TARGET_DIR. You can compile it with:"
